9f8d93316e159068a7d3a2b62efb3b7001b44727
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-03-08 14:18:27 +0900

    Call API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use reqwest::Error;
+use std::env;
+// use std::fs;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    // let contents = fs::read_to_string("articles/test.md").unwrap();
+    let endpoint = env::var("QIITA_API_ENDPOINT").unwrap() + "/items";
+    let res = reqwest::get(endpoint).await?.text().await?;
+    // let items = res.text().await?;
+    println!("{:?}", res);
+    Ok(())
 }
