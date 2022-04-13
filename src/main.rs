fe0c23f40a74050f1b7ac1fe68ff07c3e6f1299e
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-04-13 23:25:05 +0900

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,8 @@ use std::fs;
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(bound(deserialize = "'de: 'static"))]
 struct Post {
-    title: String,
+    header: PostHeader,
     body: String,
-    private: bool,
-    tags: Vec<HashMap<&'static str, String>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -34,18 +32,18 @@ struct YamlPostHeader {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     set_config();
-    let post = set_post(read_header(), set_body());
+    let json_post = make_json_post(read_post());
 
     let client = reqwest::Client::new();
     let endpoint = env::var("QIITA_API_ENDPOINT").unwrap() + "/items";
     let authorization = String::from("Bearer ") + &env::var("QIITA_API_TOKEN").unwrap().to_string();
 
-    println!("{:?}", &post);
+    println!("{:?}", &json_post);
 
     let res = client
         .post(endpoint)
         .header("Authorization", authorization)
-        .json(&post)
+        .json(&json_post)
         .send()
         .await?;
 
@@ -54,29 +52,23 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn set_post(header: PostHeader, body: String) -> Value {
-    let post = Post {
-        title: header.title,
-        private: header.private,
-        tags: header.tags,
-        body: body,
-    };
-    json!(post)
-}
-
-fn set_body() -> String {
-    let content = fs::read_to_string("articles/test.md").unwrap();
-    content
+// api post用にjson化
+fn make_json_post(post: Post) -> Value {
+    json!({
+        "title": post.header.title,
+        "private": post.header.private,
+        "tags": post.header.tags,
+        "body": post.body
+    })
 }
 
 fn set_config() {
     dotenv().ok();
 }
 
-fn read_header() -> PostHeader {
-    let post = fs::read_to_string("articles/test.md").unwrap();
-    let split_str: Vec<&str> = post.split("---").collect();
-    let yaml_post_header: YamlPostHeader = serde_yaml::from_str(&split_str[1]).unwrap();
+// 記事のheader
+fn read_header(md_header: &str) -> PostHeader {
+    let yaml_post_header: YamlPostHeader = serde_yaml::from_str(md_header).unwrap();
     let tags = make_tags_hashmap_vector(yaml_post_header.tags);
     let header = PostHeader {
         title: yaml_post_header.title,
@@ -86,8 +78,28 @@ fn read_header() -> PostHeader {
     header
 }
 
+// 記事のbody
+fn read_body(md_body: &str) -> String {
+    md_body.to_string()
+}
+
+// 記事(header+body)
+fn read_post() -> Post {
+    let post = fs::read_to_string("articles/test.md").unwrap();
+    let split_str: Vec<&str> = post.split("---").collect();
+
+    let header = read_header(&split_str[1]);
+    let body = read_body(&split_str[2]);
+    Post {
+        header: header,
+        body: body,
+    }
+}
+
 // ["tag1", "tag2"] から [{"name": "tag1"}, {"name": "tag2"}] を生成
-fn make_tags_hashmap_vector(yaml_post_header_tags: Vec<String>) -> Vec<HashMap<&'static str, String>> {
+fn make_tags_hashmap_vector(
+    yaml_post_header_tags: Vec<String>,
+) -> Vec<HashMap<&'static str, String>> {
     let mut tags: Vec<HashMap<&'static str, String>> = Vec::new();
     for tag in yaml_post_header_tags.iter() {
         let mut new_tag = HashMap::new();
